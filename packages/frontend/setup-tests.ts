92b2ed2686531c388fbfe12bc309ad8ec84d0a22
Bartosz <burtekdotryw@gmail.com>
2026-03-16 17:15:12 +0100

    chore: fix eslint issues

--- packages/frontend/setup-tests.ts
+++ packages/frontend/setup-tests.ts
@@ -4,7 +4,12 @@ import '@testing-library/jest-dom/vitest';
 
 // Mock the ResizeObserver
 const ResizeObserverMock = vitest.fn(class ResizeObserverMockImpl {
-    constructor(private readonly callback: ResizeObserverCallback) {
+    // eslint-disable-next-line @typescript-eslint/parameter-properties
+    private readonly callback: ResizeObserverCallback;
+
+    // eslint-disable-next-line promise/prefer-await-to-callbacks
+    constructor(callback: ResizeObserverCallback) {
+        this.callback = callback;
     }
 
     observe = vitest.fn(_target => {
